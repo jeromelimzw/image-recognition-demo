598a69e5309d42e56a26eaae11c9cb25ea5b34ba
jeromelimzw <jerome.lim.zw@gmail.com>
2019-05-07 23:02:56 +0800

    add option to feed random image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import Clarifai from "clarifai";
-
 import ImageRecognition from "./components/FaceRecognition";
 import ImageInputForm from "./components/ImageInput";
-
+import "./App.css";
+import { Button } from "semantic-ui-react";
+import randomImage from "./static/exampleImages";
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
   apiKey: process.env.REACT_APP_CLARIFAI_KEY
 });
+const key = process.env.REACT_APP_KEY;
 
 class App extends Component {
   constructor() {
@@ -15,7 +17,8 @@ class App extends Component {
     this.state = {
       input: "",
       imageUrl: "",
-      box: {}
+      box: {},
+      password: ""
     };
   }
 
@@ -42,6 +45,8 @@ class App extends Component {
   };
 
   handleSubmit = () => {
+    if (this.state.password !== process.env.REACT_APP_KEY)
+      return alert("password is required");
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
@@ -55,13 +60,31 @@ class App extends Component {
       });
   };
 
+  handleRandom = async () => {
+    await this.setState({ input: "" });
+    await this.setState({
+      input: randomImage()
+    });
+    await this.handleSubmit();
+  };
+
+  handlePassword = async event => {
+    await this.setState({ password: event.target.value });
+  };
+
   render() {
     const { imageUrl, box } = this.state;
-    const { handleSubmit, handleInput } = this;
+    const { handleSubmit, handleInput, handleRandom, handlePassword } = this;
     return (
-      <div className="center tc w-30  mt7">
-        <h1>Face Recognition</h1>
-        <ImageInputForm handleInput={handleInput} handleSubmit={handleSubmit} />
+      <div className="center tc w-40  mt4">
+        <h1>Face Recognition App</h1>
+        <ImageInputForm
+          handleInput={handleInput}
+          handleSubmit={handleSubmit}
+          handleRandom={handleRandom}
+          handlePassword={handlePassword}
+        />
+
         <ImageRecognition box={box} imageUrl={imageUrl} />
       </div>
     );
---
+++ src/static/exampleImages.js
@@ -0,0 +1,21 @@
+const images = [
+  "https://www.sportengland.org/media/12929/dwarf-sports.jpg",
+  "https://www.rnib.org.uk/sites/default/files/Y_41.jpg",
+  "https://australianbananas.com.au/files/images/billyslater_background.jpg",
+  "http://1.bp.blogspot.com/-jLaydXMKi1c/Vc50z9dgmSI/AAAAAAAAlJA/_kCzgYMAMaw/s1600/wYn7JHh.jpg",
+  "https://image.dhgate.com/0x0/f2/albu/g6/M00/2B/BB/rBVaR1oyR5OAM-5iAAOwwm5Z8Zc116.jpg",
+  "https://www.dhresource.com/0x0s/f2-albu-g6-M01-6B-53-rBVaR1q8UoGABrFuAADQH4HodmU596.jpg/eva-asuka-cosplay-costume-3d-print-anime.jpg",
+  "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwif2IXAz4niAhXD8HMBHec4B_gQjRx6BAgBEAU&url=https%3A%2F%2Fwww.facultyfocus.com%2Farticles%2Fteaching-and-learning%2Fstudents-perceive-feedback%2F&psig=AOvVaw0AR7eAgat4r5SwImSJu0H0&ust=1557325441828340",
+  "https://s16815.pcdn.co/wp-content/uploads/2016/04/iStock_000060783206_Medium.160415.jpg",
+  "https://tafttribune.org/wp-content/uploads/2018/10/IMG_0252-e1540599656349-900x675.jpg",
+  "https://cdn.psychologytoday.com/sites/default/files/styles/image-article_inline_full/public/field_blog_entry_teaser_image/2018-11/adobestock_169796178.jpeg?itok=g-2UJbGt",
+  "https://schools.au.reachout.com/-/media/schools/images/legacy-articles/two-boys-in-school-uniform-laughing.jpg",
+  "https://amp.businessinsider.com/images/5914aece1442931f008b475c-750-375.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/2/26/Mam_people.jpg"
+];
+
+const randomImage = () => {
+  return images[Math.round(Math.random() * images.length + 1)];
+};
+
+export default randomImage;
