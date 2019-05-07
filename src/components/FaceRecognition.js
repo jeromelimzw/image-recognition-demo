0f0361a0f4e5636112d424c5bacf7b6b8fce1d29
jeromelimzw <jerome.lim.zw@gmail.com>
2019-05-07 23:03:20 +0800

    add minor password verification to prevent misuse

--- src/components/FaceRecognition.js
+++ src/components/FaceRecognition.js
@@ -3,23 +3,21 @@ import { Image } from "semantic-ui-react";
 
 const FaceRecognition = ({ imageUrl, box }) => {
   return (
-    <div className=" ">
-      <div className=" absolute">
-        <Image id="inputimage" alt="" src={imageUrl} width="600px" />
-        {Object.entries(box).length !== 0 ? (
-          <div
-            className="absolute pointer ba b--blue bw2"
-            style={{
-              top: box.topRow,
-              right: box.rightCol,
-              bottom: box.bottomRow,
-              left: box.leftCol
-            }}
-          />
-        ) : (
-          undefined
-        )}
-      </div>
+    <div className=" absolute">
+      <Image id="inputimage" alt="" src={imageUrl} className="imgBox" />
+      {Object.entries(box).length !== 0 ? (
+        <div
+          className="absolute pointer ba b--blue bw2"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol
+          }}
+        />
+      ) : (
+        undefined
+      )}
     </div>
   );
 };
--- src/components/ImageInput.js
+++ src/components/ImageInput.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Input, Button } from "semantic-ui-react";
 
-const ImageInput = ({ handleInput, handleSubmit }) => {
+const ImageInput = ({
+  handleInput,
+  handleSubmit,
+  handleRandom,
+  handlePassword
+}) => {
   return (
-    <div className="center flex justify-between">
+    <div className="center flex justify-between mb5">
       <Input
         onChange={handleInput}
         className="w-100"
         placeholder="paste image URL here"
       />
+      <Input
+        onChange={handlePassword}
+        type="password"
+        placeholder="enter password here"
+      />
       <Button onClick={handleSubmit}>Detect</Button>
+      <Button onClick={handleRandom}>Random </Button>
     </div>
   );
 };
